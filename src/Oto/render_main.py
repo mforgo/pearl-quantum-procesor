c34e6043ef32f0b89bc694f13b9da261496c27f3
mostodont <otoskypala0@gmail.com>
2025-08-03 21:37:26 +0200

    just testing

--- src/Oto/render_main.py
+++ src/Oto/render_main.py
@@ -1,13 +1,3 @@
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((640, 480))
-pygame.display.set_caption("Simple Window")
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-pygame.quit()
+print("Rendering main module...")
