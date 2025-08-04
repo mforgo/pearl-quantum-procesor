78a2c4927ce8adadf5b62ef5e2792fbb0826796a
mostodont <otoskypala0@gmail.com>
2025-08-04 20:49:25 +0200

    added simple background letter rendering

--- src/rendering/background.py
+++ src/rendering/background.py
@@ -4,11 +4,11 @@ from random import randint
 class Background:
     def __init__(self, screen, base_color=(0, 1, 0)):
         self.screen = screen
-        self.black = (0, 0, 0)
         self.base_color = base_color
+        self.black = self.__color(0)
         self.filling_percentage = 50
         self.screen_size = screen.get_size()
-        self.char_size = self.screen_size[0] // 100
+        self.char_size = self.screen_size[0] // 10
 
     def __color(self, percentage):
         """Returns a color based on the percentage."""
@@ -27,8 +27,27 @@ class Background:
         pass
 
     def render(self):
-        pass
+        self.screen.fill(self.black)
+        for i in range(0, self.screen_size[0], self.char_size):
+            for j in range(0, self.screen_size[1], self.char_size):
+                letter = self.random_letter(randint(0, 93))
+                color = self.__color(self.filling_percentage)
+                font = pygame.font.SysFont(None, self.char_size)
+                text = font.render(letter, True, color)
+                self.screen.blit(text, (i, j))
 
     def random_letter(self, index):
         """Returns a random letter based on the index."""
-        return chr(index)
+        return chr(index % 94 + 33)  # 33 - 126
+
+pygame.init()
+pygame.font.init()
+
+test = Background(pygame.display.set_mode((800, 600)))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    test.render()
+    pygame.display.flip()
