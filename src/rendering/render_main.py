ece172805e5ab97eb49944905beba01d959d60af
mostodont <otoskypala0@gmail.com>
2025-08-06 21:59:37 +0200

    added half of the rendering

--- src/rendering/render_main.py
+++ src/rendering/render_main.py
@@ -10,6 +10,17 @@ class RenderMain:
         self.running = True
         self.base_color = (0, 1, 0)
         self.background = background.Background(self.screen, self.base_color)
+        self.code_window = frontground.Code_window(self.screen, (40.73, 55.55), pos=(11.11, 11.11), base_color=self.base_color)
+        self.register_window = [
+            frontground.RegisterWindow(self.screen, (14.82, 4.35), pos=(59.26, 13.04), base_color=self.base_color),
+            frontground.RegisterWindow(self.screen, (14.82, 4.35), pos=(59.26, 21.74), base_color=self.base_color),
+            frontground.RegisterWindow(self.screen, (14.82, 4.35), pos=(59.26, 30.43), base_color=self.base_color),
+            frontground.RegisterWindow(self.screen, (14.82, 4.35), pos=(59.26, 39.13), base_color=self.base_color),
+            frontground.RegisterWindow(self.screen, (14.82, 4.35), pos=(77.78, 13.04), base_color=self.base_color),
+            frontground.RegisterWindow(self.screen, (14.82, 4.35), pos=(77.78, 21.74), base_color=self.base_color),
+            frontground.RegisterWindow(self.screen, (14.82, 4.35), pos=(77.78, 30.43), base_color=self.base_color),
+            frontground.RegisterWindow(self.screen, (14.82, 4.35), pos=(77.78, 39.13), base_color=self.base_color),
+            ]
         self.clock = pygame.time.Clock()
 
     def run(self):
@@ -34,15 +45,28 @@ class RenderMain:
 
     def __render(self):
         self.background.render()
+        self.code_window.render()
+        for window in self.register_window:
+            window.render("123456789ABCDEF")
         pygame.display.update()
         pygame.display.flip()
     
     def __handle_events(self):
         for event in pygame.event.get():
+            self.code_window.handle_event(event)
             if event.type == pygame.QUIT:
                 self.running = False
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 self.running = False
+            if event.type == pygame.VIDEORESIZE:
+                self.code_window._reinit()
+                for window in self.register_window:
+                    window._reinit()
 
-test = RenderMain()
-test.run()
+
+def main():
+    render_main = RenderMain()
+    render_main.run()
+
+if __name__ == "__main__":
+    main()
